215823aebf0e37fb8530abcf037c9ac128425361
AbdultechX <125444167+AbdulTechX@users.noreply.github.com>
2024-08-23 22:40:14 +0100

    Add files via upload
    Modify the App

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Holberton_Logo from './Holberton_Logo.jpg'; // Assuming this is your image file
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={Holberton_Logo} alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor='email'>Email:</label>
+        <input type='email' id='email'></input>
+        <br></br>
+        <label>Password:</label>
+        <input type='password' id='password'></input>
+        <br></br>
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
